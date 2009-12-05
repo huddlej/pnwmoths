3e18973d9867caf60dfef1983c134652308f6168
John.L.Huddleston <John.L.Huddleston@806f109c-c8c0-11dd-a880-a1081c7ac358>
2009-12-05 00:06:00 +0000

    updating map to reflect the map on the production server
    git-svn-id: https://pnwmoths.googlecode.com/svn/trunk@106 806f109c-c8c0-11dd-a880-a1081c7ac358

--- couchdb/views/by_species/map.js
+++ couchdb/views/by_species/map.js
@@ -1,5 +1,33 @@
 function(doc) {
-    if (doc.genus && doc.species) {
-        emit(doc.genus + " " + doc.species, null);
+    var longitude_precision = 0,
+        latitude_precision = 0;
+
+    if(doc.elevation) {
+        doc.elevation = parseInt(doc.elevation);
     }
+    else {
+        doc.elevation = "";
+    }
+
+    if(doc.longitude) {
+        try {
+            longitude_precision = doc.longitude.split(".")[1].length;
+        }
+        catch (e) {
+        }
+        doc.longitude = Number(doc.longitude).toFixed(1);
+    }
+
+    if(doc.latitude) {
+        try {
+            latitude_precision = doc.latitude.split(".")[1].length;
+        }
+        catch (e) {
+        }
+        doc.latitude = Number(doc.latitude).toFixed(1);
+    }
+
+    doc.precision = Math.min(longitude_precision, latitude_precision);
+    doc.site_name = doc.city;
+    emit(doc.genus + " " + doc.species, null);
 }
