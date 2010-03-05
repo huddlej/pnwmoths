a9aa5df2b6db5b64823df294fce1a51661d6176c
John Huddleston <huddlej@gmail.com>
2010-03-05 13:55:29 -0800

    stubbed out similar species

---
+++ www/data/SimilarSpecies.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Represents a single instance of a similar species.
+ */
+class PNWMoths_Data_SimilarSpecies extends PNWMoths_Data {
+    protected var $original_species;
+    protected var $species;
+    protected var $images;
+
+    public function getData() {
+    }
+}
+?>
