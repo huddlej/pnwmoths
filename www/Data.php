95e42d8775efba331def953206d61f2d70badf62
John Huddleston <huddlej@gmail.com>
2010-03-05 13:54:54 -0800

    reorged db connection logic. this is going to go away as I move toward proper Zend structure.

---
+++ www/Data.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Setup include paths and Zend autoloader so all required modules will be
+ * loaded automatically.
+ */
+set_include_path("/usr/share/php/libzend-framework-php" . PATH_SEPARATOR . "/home/huddlej/src/svn.tillikum.org/trunk/www/library" . PATH_SEPARATOR . get_include_path());
+require_once 'Zend/Loader/Autoloader.php';
+$loader = Zend_Loader_Autoloader::getInstance();
+$loader->registerNamespace('Tillikum_');
+
+class PNWMoths_Data {
+    protected function getDatabase() {
+        $couchdb = new Tillikum_CouchDb("http://localhost:5984");
+        $db = new Tillikum_CouchDb_Database($couchdb, "pnwmoths");
+        return $db;
+    }
+
+    public function getData(array $params = array()) {
+        throw new Exception("Not defined.");
+    }
+}
+?>
--- www/data/Map.php
+++ www/data/Map.php
@@ -1,31 +1,22 @@
 <?php
-/**
- * Setup include paths and Zend autoloader so all required modules will be
- * loaded automatically.
- */
-set_include_path("/usr/share/php/libzend-framework-php" . PATH_SEPARATOR . "/home/huddlej/src/svn.tillikum.org/trunk/www/library" . PATH_SEPARATOR . get_include_path());
-require_once 'Zend/Loader/Autoloader.php';
-$loader = Zend_Loader_Autoloader::getInstance();
-$loader->registerNamespace('Tillikum_');
+class PNWMoths_Data_Map extends PNWMoths_Data {
+    public function getData(array $params = array()) {
+        $db = $this->getDatabase();
 
-/**
- * Connect to the database.
- */
-$couchdb = new Tillikum_CouchDb("http://localhost:5984");
-$db = new Tillikum_CouchDb_Database($couchdb, "pnwmoths");
+        /**
+         * Fetch species data. A species key is optional.
+         */
+        $viewParams = array();
+        if (array_key_exists("species", $params)) {
+            $viewParams["key"] = Zend_Json::encode($params["species"]);
+        }
+        $data = $db->getView("moths", "by_species", $viewParams);
 
-/**
- * Fetch species data. A species key is optional.
- */
-$viewParams = array();
-if (array_key_exists("species", $_GET)) {
-    $viewParams["key"] = Zend_Json::encode($_GET["species"]);
+        /**
+         * Encode species data as JSON for use by javascript.
+         */
+        $response = Zend_Json::encode($data);
+        return $response;
+    }
 }
-$data = $db->getView("moths", "by_species", $viewParams);
-
-/**
- * Encode species data as JSON for use by javascript.
- */
-$response = Zend_Json::encode($data);
-print $response;
 ?>
