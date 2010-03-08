f90ce3f32fb119ec1cc7e583bc74f9bafab2ee5e
John Huddleston <huddlej@gmail.com>
2010-03-08 15:20:35 -0800

    updated the similar species test page to work with the new output of the data class.

--- www/data/test_similar_species.php
+++ www/data/test_similar_species.php
@@ -6,37 +6,28 @@ if (array_key_exists("species", $_GET)) {
 }
 
 $model = new PNWMoths_Data_SimilarSpecies();
-$all_species = $model->getAll();
-
-// foreach($all_species as $s) {
-//     if ($s->key != $species) {
-//         print "<a href='?class=SimilarSpecies&species=".$s->key."'>" . $s->key . "</a><br />";
-//     }
-// }
 
 if (isset($species)) {
     $data = $model->getData(array("species" => $species));
 
     print "<h2>Species similar to $species</h2>";
     foreach($data as $row) {
-        foreach($row->doc->similar_species as $similar_species) {
-            print "<p>{$similar_species->species}</p>";
-            if (isset($similar_species->images)) {
-                print "<ul>";
-                foreach($similar_species->images as $image_id) {
-                    print "<li><img src='getFile.php?doc_id=".$row->id."&attachment_id=".$image_id."' /></li>";
-                }
-                print "</ul>";
-            }
+        print "<p>{$row["species"]}</p>";
+        print "<ul>";
+        foreach($row["images"] as $image_url) {
+            print "<li><img src='$image_url' /></li>";
         }
+        print "</ul>";
     }
 }
 
-// if (isset($data)) {
-//     if (isset($data)) {
-//         print "<pre>";
-//         print_r($data);
-//         print "</pre>";
-//     }
-// }
+if (array_key_exists("debug", $_GET)) {
+    if (isset($data)) {
+        if (isset($data)) {
+            print "<pre>";
+            print_r($data);
+            print "</pre>";
+        }
+    }
+}
 ?>
