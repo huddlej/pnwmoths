91a7d23b1a11180e0f76f9b456f17484cb448f30
John Huddleston <huddlej@gmail.com>
2010-03-08 15:18:41 -0800

    removed the getAll method and cleaned up the output of the getData method to a list of associative arrays containing the similar species name and a list of image urls.

--- www/data/SimilarSpecies.php
+++ www/data/SimilarSpecies.php
@@ -16,31 +16,23 @@ class PNWMoths_Data_SimilarSpecies extends PNWMoths_Data {
 
         $species = Zend_Json::encode($params["species"]);
         $viewParams = array("include_docs" => 'true',
-                            "reduce" => 'false',
                             "key" => $species);
 
         $results = $db->getView($this->designDoc, $this->viewName,
                                 $viewParams);
 
-//         $attachments = array();
-//         foreach($results->rows as $row) {
-//             foreach($row->doc->similar_species as $similar_species) {
-//                 foreach($similar_species->images as $image_id) {
-//                     $attachments[$image_id] = $db->getAttachment($row->id, $image_id);
-//                 }
-//             }
-//         }
-        
-//         return $attachments;
-        return $results->rows;
-    }
+        $similar_species = array();
+        foreach ($results->rows as $row) {
+            $images = array();
+            foreach ($row->doc->_attachments as $attachment_id => $attachment) {
+                // TODO: make URL a property
+                $images[] = "http://localhost/~huddlej/getFile.php?doc_id={$row->id}&attachment_id={$attachment_id}";
+            }
+            $similar_species[] = array("species" => $row->doc->similar_species,
+                                       "images" => $images);
+        }
 
-    public function getAll() {
-        $db = $this->getDatabase();
-        $viewParams = array('group' => 'true');
-        $results = $db->getView($this->designDoc, $this->viewName,
-                                $viewParams);
-        return $results->rows;
+        return $similar_species;
     }
 }
 ?>
