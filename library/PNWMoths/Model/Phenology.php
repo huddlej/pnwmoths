1c7ad1668faba75eea4f028c65eb206e4922ee81
John Huddleston <huddlej@gmail.com>
2010-03-15 16:07:59 -0700

    added basic (read: stupid) implementation of phenology model with samples indexed by month.

--- library/PNWMoths/Model/Phenology.php
+++ library/PNWMoths/Model/Phenology.php
@@ -1,6 +1,34 @@
 <?php
 class PNWMoths_Model_Phenology extends PNWMoths_Model {
     public function getData(array $params = array()) {
+        if (array_key_exists("species", $params) === false) {
+            return array();
+        }
+
+        $species = $params["species"];
+        $samplesModel = new PNWMoths_Model_SpeciesSample();
+        $samples = $samplesModel->getData(array("species" => $species));
+
+        $samplesByInterval = array();
+
+        /**
+         * Pre-populate samples by interval with zeroes.
+         */
+        $startInterval = 1;
+        $endInterval = 12;
+        $samplesByInterval = array_fill($startInterval, $endInterval, 0);
+
+        /**
+         * Map sample data to the given interval by counting each sample that
+         * matches an interval marker.
+         */
+        foreach ($samples as $sample) {
+            if ($sample->month) {
+                $samplesByInterval[(int)$sample->month] += 1;
+            }
+        }
+
+        return $samplesByInterval;
     }
 }
 ?>
