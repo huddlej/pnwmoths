5fdb3c4ebc9f5cbf252efdd7ff7003dee1ff6bf6
John Huddleston <huddlej@gmail.com>
2010-03-15 14:38:19 -0700

    added a bootstrap file to setup the right paths and zend autoloader. this will eventually all go in an ini file.

---
+++ www/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Setup include paths and Zend autoloader so all required modules will be
+ * loaded automatically.
+ */
+$paths = array(
+    "/usr/share/php/libzend-framework-php",
+    "/home/huddlej/src/tillikum-couchdb/library",
+    "/home/huddlej/pnwmoths/library",
+    get_include_path()
+);
+    
+set_include_path(implode(PATH_SEPARATOR, $paths));
+
+require_once 'Zend/Loader/Autoloader.php';
+$loader = Zend_Loader_Autoloader::getInstance();
+$loader->registerNamespace('Tillikum_');
+$loader->registerNamespace('PNWMoths_');
+?>
