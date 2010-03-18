1febe970f1276f886fd7c69512d87d52f05aab58
John Huddleston <huddlej@gmail.com>
2010-03-17 17:21:01 -0700

    replaced Zend_Rest_Server instance with switch statement and Zend_Json::encode. it's worth it to have less work in javascript converting XML to JSON.

--- www/service.php
+++ www/service.php
@@ -10,11 +10,25 @@ function getSamples($species) {
 function getPhenology($species) {
     $model = new PNWMoths_Model_Phenology();
     $phenology = $model->getData(array("species" => $species));
-    return $phenology;
+    return array_values($phenology);
 }
 
-$server = new Zend_Rest_Server();
-$server->addFunction('getSamples');
-$server->addFunction('getPhenology');
-$server->handle();
+if (array_key_exists("method", $_GET) && array_key_exists("species", $_GET)) {
+    $species = $_GET["species"];
+
+    switch ($_GET["method"]) {
+        case 'getPhenology':
+            $data = getPhenology($species);
+            break;
+        case 'getSamples':
+            $data = getSamples($species);
+            break;
+        default:
+            break;
+    }
+
+    if (isset($data)) {
+        echo Zend_Json::encode($data);
+    }
+}
 ?>
