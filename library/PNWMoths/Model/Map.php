335e6b1fed7b06deb999b7b71f6ca839d432880b
John Huddleston <huddlej@gmail.com>
2010-03-30 10:40:48 -0700

    update Map model to fetch samples from the database and prepare them for display in a map.

--- library/PNWMoths/Model/Map.php
+++ library/PNWMoths/Model/Map.php
@@ -1,22 +1,56 @@
 <?php
 class PNWMoths_Model_Map extends PNWMoths_Model {
     public static function getData(array $params = array()) {
-        $db = $this->getDatabase();
-
-        /**
-         * Fetch species data. A species key is optional.
-         */
-        $viewParams = array();
-        if (array_key_exists("species", $params)) {
-            $viewParams["key"] = Zend_Json::encode($params["species"]);
+        if (array_key_exists("species", $params) === false) {
+            return array();
         }
-        $data = $db->getView("moths", "by_species", $viewParams);
 
-        /**
-         * Encode species data as JSON for use by javascript.
-         */
-        $response = Zend_Json::encode($data);
-        return $response;
+        $species = $params["species"];
+        $samples = PNWMoths_Model_SpeciesSample::getData(
+            array("species" => $species)
+        );
+
+        // Create a unique list of points indexed by latitude|longitude.
+        $points = array();
+        foreach ($samples as $sample) {
+            $point = implode("|", array($sample->latitude, $sample->longitude));
+
+            if (array_key_exists($point, $points)) {
+            }
+            else {
+                $points[$point] = array(
+                    "lat" => (float)$sample->latitude,
+                    "lng" => (float)$sample->longitude,
+                    "info" => array(
+                        "site_name" => $sample->site_name,
+                        "county" => $sample->county,
+                        "state" => $sample->state,
+                        "elevation" => $sample->elevation
+                    )
+                );
+            }
+        }
+
+        foreach ($points as $point_index => $point) {
+            $html = "<h2>$species</h2>";
+            $html .= "<table>";
+            foreach ($point["info"] as $key => $value) {
+                if ($value === null) {
+                    $clean_value = "";
+                }
+                else {
+                    $clean_value = $value;
+                }
+                $html .= "<tr>";
+                $html .= "<td>$key</td>";
+                $html .= "<td>$clean_value</td>";
+                $html .= "</tr>";
+            }
+            $html .= "</table>";
+            $points[$point_index]["info"] = $html;
+        }
+
+        return array_values($points);
     }
 }
 ?>
