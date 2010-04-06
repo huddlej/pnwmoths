67947c809f79950c644fdbbef1963ba50215bda2
John Huddleston <huddlej@gmail.com>
2010-04-06 10:42:38 -0700

    added phenology toggle to map container and shortened filter toggle name.

--- dokuwiki/lib/scripts/map.js
+++ dokuwiki/lib/scripts/map.js
@@ -46,13 +46,20 @@ function Map() {
     mgr = new MarkerManager(map);
 
     // Add filters to map container.
-    var filtersTab = jQuery("<div><a href='' id='toggle-filters'>Toggle Filters</a></div>");
+    var filtersTab = jQuery("<div><a href='' id='toggle-filters'>Filters</a></div>");
     filtersTab.css("background-color", "beige");
     filtersTab.css("position", "relative");
     filtersTab.css("width", "80px");
+
+    var phenologyTab = jQuery("<div><a href='' id='toggle-phenology'>Phenology</a></div>");
+    phenologyTab.css("background-color", "beige");
+    phenologyTab.css("position", "relative");
+    phenologyTab.css("width", "80px");
+
     map.getContainer().appendChild(filtersTab.get(0));
+    map.getContainer().appendChild(phenologyTab.get(0));
     map.getContainer().appendChild(jQuery("#filters").get(0));
-    //map.getContainer().appendChild(jQuery("#plot").get(0));
+    map.getContainer().appendChild(jQuery("#plot").get(0));
 }
 
 function createMarkers(data) {
