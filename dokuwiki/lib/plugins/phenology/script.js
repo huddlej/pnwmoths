fb79df2cf808f60b98593a63885e3b508460addd
John Huddleston <huddlej@gmail.com>
2010-04-09 11:58:08 -0700

    merged speciesData js into phenology js.

--- dokuwiki/lib/plugins/phenology/script.js
+++ dokuwiki/lib/plugins/phenology/script.js
@@ -1,11 +1,17 @@
-var map, mgr;
+var map,
+    mgr,
+    species,
+    data,
+    filters = {};
 
 jQuery(document).ready(function () {
-    var species = jQuery("#species").hide().text(),
-        newmap = new Map();
+    var newmap = new Map();
+    species = jQuery("#species").hide().text();
 
-    // Listen for the custom "dataIsReady" event which indicates the JSON data
-    // from the server is ready for use.
+    // Setup custom events "requestData" and "dataIsReady". The latter initiates
+    // a request to the data service passing any filters that have been
+    // set. When the data is ready, the "dataIsReady" event is triggered.
+    jQuery(document).bind("requestData", function (event) { getData(species, filters); });
     jQuery(document).bind("dataIsReady", preparePhenologyData);
     jQuery(document).bind(
         "dataIsReady",
@@ -13,7 +19,96 @@ jQuery(document).ready(function () {
             createMarkers(groupMarkerData(data));
         }
     );
+
+    //
+    // Setup filters.
+    //
+
+    // All filters
+    jQuery("#clear-filters").click(
+        function (event) {
+            event.preventDefault();
+            filters = {};
+            jQuery("#filters input:text").val("");
+            jQuery(document).trigger("requestData");
+        }
+    );
+
+    // Elevation
+    jQuery("#form-elevation").submit(
+        function (event) {
+            event.preventDefault();
+            var start = jQuery("#startelevation").val(),
+                end = jQuery("#endelevation").val();
+            filters["elevation"] = [start, end];
+            jQuery(document).trigger("requestData");
+        }
+    );
+    jQuery("#clear-filter-elevation").click(
+        function (event) {
+            event.preventDefault();
+            if (filters.hasOwnProperty("elevation")) {
+                delete filters["elevation"];
+                jQuery(this).siblings("input:text").val("");
+                jQuery(document).trigger("requestData");
+            }
+        }
+    );
+
+    // Date
+    jQuery("#form-date").submit(
+        function (event) {
+            event.preventDefault();
+            var start = jQuery("#startdate").val(),
+                end = jQuery("#enddate").val();
+            filters["date"] = [start, end];
+            jQuery(document).trigger("requestData");
+        }
+    );
+    jQuery("#clear-filter-date").click(
+        function (event) {
+            event.preventDefault();
+            if (filters.hasOwnProperty("date")) {
+                delete filters["date"];
+                jQuery(this).siblings("input:text").val("");
+                jQuery(document).trigger("requestData");
+            }
+        }
+    );
+
+    // Trigger the initial request for data.
+    jQuery(document).trigger("requestData");
 });
+
+// Requests data from the data service for the given species and filtering by
+// the given filters. Filtering takes place on the server before the data is
+// returned.
+function getData(species, filters) {
+    var key,
+        requestData = {
+            "method": "getSamples",
+            "species": species
+        };
+
+    // Add filter values to the request data.
+    for (key in filters) {
+        if (filters.hasOwnProperty(key)) {
+           requestData[key] = filters[key];
+        }
+    }
+
+    jQuery.getJSON(
+        "http://localhost/~huddlej/service.php",
+        requestData,
+        function (new_data, textStatus) {
+            // Update global data variable.
+            data = new_data;
+
+            // Trigger "data is ready" event.
+            jQuery(document).trigger("dataIsReady");
+        }
+    );
+}
 
 function preparePhenologyData(event) {
     var phenologyData = [],
