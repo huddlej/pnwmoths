a22a90e80271fbc0eb6958ef3eacf80b6666dc1b
John Huddleston <huddlej@gmail.com>
2010-04-09 11:58:52 -0700

    removed speciesData js and all references to it.

--- dokuwiki/lib/tpl/pnwmoths/main.php
+++ dokuwiki/lib/tpl/pnwmoths/main.php
@@ -44,7 +44,6 @@ if (!defined('DOKU_INC')) die();
     <script language="javascript" type="text/javascript" src="/~huddlej/js/jqplot.canvasTextRenderer.min.js"></script>
     <script language="javascript" type="text/javascript" src="/~huddlej/js/jqplot.canvasAxisLabelRenderer.min.js"></script>
     <script language="javascript" type="text/javascript" src="/~huddlej/js/jqplot.categoryAxisRenderer.min.js"></script>
-    <script language="javascript" type="text/javascript" src="/~huddlej/js/speciesData.js"></script>
 
     <!--[if IE]><script language="javascript" type="text/javascript" src="/~huddlej/js/excanvas.min.js"></script><![endif]-->
     <link rel="stylesheet" href="/~huddlej/css/jquery.jqplot.min.css" type="text/css">
