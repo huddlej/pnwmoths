9296819bb7af3b730b28748507af27c052654ead
John Huddleston <huddlej@gmail.com>
2010-04-27 08:38:00 -0700

    renamed variables to fit project standard.

--- couchdb/lists/images.js
+++ couchdb/lists/images.js
@@ -1,19 +1,19 @@
 function (head, req) {
     provides("html", function () {
         var row,
-            imageUrl,
+            image_url,
             attachment,
-            imageSrc;
+            image_src;
 
         // The image url provided in the GET query tells the list where to find
         // the attached images from a web-accessible path.
         if (req.query.image_url) {
-            imageUrl = req.query.image_url;
+            image_url = req.query.image_url;
             while (row = getRow()) {
                 if (row.doc) {
                     for (attachment in row.doc._attachments) {
-                        imageSrc = imageUrl + "?id=" + row.id + "/" + attachment;
-                        send("<li><a href='" + imageSrc + "'><img src='" + imageSrc + "' /></a></li>");
+                        image_src = image_url + "?id=" + row.id + "/" + attachment;
+                        send("<li><a href='" + image_src + "'><img src='" + image_src + "' /></a></li>");
                      }
                 }
             }
