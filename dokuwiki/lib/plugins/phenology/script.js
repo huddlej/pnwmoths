b17b7d3c5a051b000c8b6ecd4df2990eaef9812f
John Huddleston <huddlej@gmail.com>
2010-05-11 08:52:37 -0700

    moved Chart class into PNWMOTHS.Chart declaration.

--- dokuwiki/lib/plugins/phenology/script.js
+++ dokuwiki/lib/plugins/phenology/script.js
@@ -274,6 +274,119 @@ PNWMOTHS.Map = function () {
 }();
 PNWMOTHS.Chart = function () {
     return {
+        initialize: function (data) {
+            var phenologyData = [],
+                flatPhenologyData = [],
+                startInterval = 0,
+                endInterval = 12,
+                daysPerSegment = 10,
+                maxSegments = 2,
+                i, j,
+                plot,
+                month, segment,
+                ticks = ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"],
+                dataLabels = [],
+                tick;
+
+            // Pre-populate samples by interval with zeros.
+            for (i = startInterval; i < endInterval; i++) {
+                    phenologyData[i] = [];
+                // One value per segment per month in the phenology.
+                for (j = 0; j <= maxSegments; j++) {
+                    phenologyData[i][j] = 0;
+                }
+            }
+
+            // Map sample data to the given interval by counting each sample
+            // that matches an interval marker.
+            for (i in data) {
+                if (data.hasOwnProperty(i) && data[i].month) {
+                    // If a record doesn't have a "day" value, don't use it. It is
+                    // better to omit a record than mislead users by defaulting the
+                    // record to the beginning of the month or some other similar
+                    // strategy.
+                    if (data[i].day) {
+                        // Records are indexed starting with 0 so all months are shifted
+                        // by 1.
+                        month = parseInt(data[i].month) - 1;
+
+                        // If a record has a day value, place it in the right segment.
+                        segment = Math.floor(parseInt(data[i].day) / daysPerSegment);
+
+                        // The graph will never display more than the max number of
+                        // segments, so days 30 and 31 get placed into the last segment.
+                        segment = Math.min(segment, maxSegments);
+
+                        // Count the number of records for this month and this segment.
+                        phenologyData[month][segment] += 1;
+                    }
+                }
+            }
+
+            // Flatten nested phenology data into a single list.
+            for (i in phenologyData) {
+                for (j in phenologyData[i]) {
+                    flatPhenologyData.push(phenologyData[i][j]);
+                }
+            }
+
+            // Prepare data for jqPlot by nesting our single data set in a list of data
+            // sets.
+            flatPhenologyData = [flatPhenologyData];
+
+            // Prepare data labels.
+
+            // Build a sequence of tick values consisting of one month letter and n
+            // empty values for all months where n is the number of segments per month
+            // in the phenology minus 1. For example: ["J", "", "", "F", "", "",...] for
+            // n=3.
+            for (tick in ticks) {
+                dataLabels.push(ticks[tick]);
+                for (i = 0; i <= maxSegments - 1; i++) {
+                    dataLabels.push(" ");
+                }
+            }
+
+            // jqPlot requires the placeholder div to be visible in the DOM when
+            // the plot is created. Each time a new plot is generated the div
+            // needs to be emptied and shown. After the plot is generated, the
+            // div can be hidden again.
+            plotDiv = jQuery("#plot");
+            plotDiv.empty();
+            plotDiv.show();
+            return PNWMOTHS.Chart.render(flatPhenologyData, dataLabels);
+        },
+        render: function (data, dataLabels) {
+            // Return a new jqPlot. This mostly consists of a lot of jqPlot
+            // options.
+            return jQuery.jqplot(
+                "plot",
+                data,
+                {
+                    seriesDefaults: {
+                        renderer: jQuery.jqplot.BarRenderer,
+                        rendererOptions: {shadowAlpha: 0, barWidth: 5}
+                    },
+                    grid: {drawGridlines: false},
+                    title: "Flight Season",
+                    axes: {
+                        xaxis: {
+                            label: 'Month',
+                            renderer: jQuery.jqplot.CategoryAxisRenderer,
+                            labelRenderer: jQuery.jqplot.CanvasAxisLabelRenderer,
+                            ticks: dataLabels
+                        },
+                        yaxis: {
+                            autoscale: true,
+                            label: 'Number of Records',
+                            labelRenderer: jQuery.jqplot.CanvasAxisLabelRenderer,
+                            tickOptions: {formatString: '%i'},
+                            showTickMarks: false
+                        }
+                    }
+                }
+            );
+        }
     };
 }();
 PNWMOTHS.Filters = function () {
@@ -294,7 +407,6 @@ jQuery(document).ready(function () {
         i, j,
         data_id, data_name;
 
-    PNWMOTHS.Chart= new Chart();
     PNWMOTHS.Map.map = PNWMOTHS.Map.initialize();
     data_name = "species-data";
     data_id = "#" + data_name;
@@ -489,124 +601,6 @@ function buildOptionFilterCallback(optionFilterName) {
                 option.text(data[i]);
                 select.append(option);
             }
-        }
-    };
-}
-
-function Chart() {
-    return {
-        initialize: function (data) {
-            var phenologyData = [],
-                flatPhenologyData = [],
-                startInterval = 0,
-                endInterval = 12,
-                daysPerSegment = 10,
-                maxSegments = 2,
-                i, j,
-                plot,
-                month, segment,
-                ticks = ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"],
-                dataLabels = [],
-                tick;
-
-            // Pre-populate samples by interval with zeros.
-            for (i = startInterval; i < endInterval; i++) {
-                    phenologyData[i] = [];
-                // One value per segment per month in the phenology.
-                for (j = 0; j <= maxSegments; j++) {
-                    phenologyData[i][j] = 0;
-                }
-            }
-
-            // Map sample data to the given interval by counting each sample
-            // that matches an interval marker.
-            for (i in data) {
-                if (data.hasOwnProperty(i) && data[i].month) {
-                    // If a record doesn't have a "day" value, don't use it. It is
-                    // better to omit a record than mislead users by defaulting the
-                    // record to the beginning of the month or some other similar
-                    // strategy.
-                    if (data[i].day) {
-                        // Records are indexed starting with 0 so all months are shifted
-                        // by 1.
-                        month = parseInt(data[i].month) - 1;
-
-                        // If a record has a day value, place it in the right segment.
-                        segment = Math.floor(parseInt(data[i].day) / daysPerSegment);
-
-                        // The graph will never display more than the max number of
-                        // segments, so days 30 and 31 get placed into the last segment.
-                        segment = Math.min(segment, maxSegments);
-
-                        // Count the number of records for this month and this segment.
-                        phenologyData[month][segment] += 1;
-                    }
-                }
-            }
-
-            // Flatten nested phenology data into a single list.
-            for (i in phenologyData) {
-                for (j in phenologyData[i]) {
-                    flatPhenologyData.push(phenologyData[i][j]);
-                }
-            }
-
-            // Prepare data for jqPlot by nesting our single data set in a list of data
-            // sets.
-            flatPhenologyData = [flatPhenologyData];
-
-            // Prepare data labels.
-
-            // Build a sequence of tick values consisting of one month letter and n
-            // empty values for all months where n is the number of segments per month
-            // in the phenology minus 1. For example: ["J", "", "", "F", "", "",...] for
-            // n=3.
-            for (tick in ticks) {
-                dataLabels.push(ticks[tick]);
-                for (i = 0; i <= maxSegments - 1; i++) {
-                    dataLabels.push(" ");
-                }
-            }
-
-            // jqPlot requires the placeholder div to be visible in the DOM when
-            // the plot is created. Each time a new plot is generated the div
-            // needs to be emptied and shown. After the plot is generated, the
-            // div can be hidden again.
-            plotDiv = jQuery("#plot");
-            plotDiv.empty();
-            plotDiv.show();
-            return PNWMOTHS.Chart.render(flatPhenologyData, dataLabels);
-        },
-        render: function (data, dataLabels) {
-            // Return a new jqPlot. This mostly consists of a lot of jqPlot
-            // options.
-            return jQuery.jqplot(
-                "plot",
-                data,
-                {
-                    seriesDefaults: {
-                        renderer: jQuery.jqplot.BarRenderer,
-                        rendererOptions: {shadowAlpha: 0, barWidth: 5}
-                    },
-                    grid: {drawGridlines: false},
-                    title: "Flight Season",
-                    axes: {
-                        xaxis: {
-                            label: 'Month',
-                            renderer: jQuery.jqplot.CategoryAxisRenderer,
-                            labelRenderer: jQuery.jqplot.CanvasAxisLabelRenderer,
-                            ticks: dataLabels
-                        },
-                        yaxis: {
-                            autoscale: true,
-                            label: 'Number of Records',
-                            labelRenderer: jQuery.jqplot.CanvasAxisLabelRenderer,
-                            tickOptions: {formatString: '%i'},
-                            showTickMarks: false
-                        }
-                    }
-                }
-            );
         }
     };
 }
