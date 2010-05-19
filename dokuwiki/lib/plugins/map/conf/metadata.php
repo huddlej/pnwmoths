ae2f2553e79aea48c5d56479bf3bf7d6c9a16b95
John Huddleston <huddlej@gmail.com>
2010-05-19 08:40:10 -0700

    added separate map plugin. history repeats itself.

---
+++ dokuwiki/lib/plugins/map/conf/metadata.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Options for the map plugin
+ *
+ * @author John Huddleston <huddlej@gmail.com>
+ */
+
+$meta['google_maps_api_key'] = array('string');
+
---
+++ dokuwiki/lib/plugins/map/lang/en/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * English language file
+ *
+ * @author     John Huddleston <huddlej@gmail.com>
+ */
+
+// for the configuration manager
+$lang['google_maps_api_key'] = 'Google Maps API Key';
