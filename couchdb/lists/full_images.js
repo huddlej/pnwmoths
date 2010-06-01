ea54c5a4bb324d5dbd6896ae88b7043c5fec63af
John Huddleston <huddlej@gmail.com>
2010-06-01 12:17:46 -0700

    moved html from js show into mustache templates

--- couchdb/lists/full_images.js
+++ couchdb/lists/full_images.js
@@ -1,45 +1,38 @@
 function (head, req) {
+    // !json templates.lists.full_images
+    // !code vendor/mustache.js
     provides("html", function () {
         var row,
-            image_url,
             image_src,
-            image,
             attachment,
-            first_row = true;
+            first_row = true,
+            view;
 
         // The image url provided in the GET query tells the list where to find
         // the attached images from a web-accessible path.
         if (req.query.image_url) {
             image_url = req.query.image_url;
 
-            send("<div id='images'>");
+            send(Mustache.to_html(templates.lists.full_images.head));
 
             while (row = getRow()) {
                 if (row.doc) {
                     for (attachment in row.doc._attachments) {
-                        image_src = image_url + "?id=" + row.id + "/" + attachment;
-                        image = "<img src='" + image_src + "' title='" + (row.doc.caption || "") + "' />";
+                        view = {"image_src": image_url + "?id=" + row.id + "/" + attachment,
+                                "title": row.doc.caption || "",
+                                "first_row": first_row,
+                                "show_title": req.query.show_title,
+                                "species": row.doc.species};
+                        send(Mustache.to_html(templates.lists.full_images.row, view));
 
                         if (first_row) {
-                            send("<div id='current-image'>" + image + "</div>");
-                            send("<ul id='other-images' class='jcarousel-skin-tango'>");
                             first_row = false;
                         }
-
-                        send("<li>");
-                        send("<a href='" + image_src + "'>");
-                        send(image);
-                        if (req.query.show_title) {
-                            send("<br />" + row.doc.species);
-                        }
-                        send("</a>");
-                        send("</li>\n");
                      }
                 }
             }
 
-            send("</ul>");
-            send("</div>");
+            send(Mustache.to_html(templates.lists.full_images.foot));
         }
     });
 }
