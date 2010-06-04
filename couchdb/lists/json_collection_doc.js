8b401f771d8b33cd83d74d417efe33f3a3cb6b58
John Huddleston <huddlej@gmail.com>
2010-06-04 11:25:06 -0700

    added logic to the collection doc couchdb list from the getSamples data method including an updated county value, a sortable date, and results sorted by date.

--- couchdb/lists/json_collection_doc.js
+++ couchdb/lists/json_collection_doc.js
@@ -1,15 +1,35 @@
 function (head, req) {
+    // !code vendor/date.js
     provides("html", function () {
         var row, rows = [];
         while (row = getRow()) {
             if (row.doc) {
+                if (row.doc.hasOwnProperty("state") && row.doc.state) {
+                    row.doc.county = row.doc.county + " (" + row.doc.state + ")";
+                }
+
                 row.doc.latitude = Number(row.doc.latitude);
                 row.doc.longitude = Number(row.doc.longitude);
                 row.doc.site_name = row.value.site_name;
                 row.doc.precision = row.value.precision;
+                row.doc.date = getSortableDate(row.doc);
                 rows.push(row.doc);
             }
         }
+
+        // Sort all records by date.
+        rows.sort(function (a, b) {
+            if (a.date == b.date) {
+                return 0;
+            }
+            else if (a.date < b.date) {
+                return -1;
+            }
+            else {
+                return 1
+            }
+        });
+
         send(toJSON(rows));
     });
 }
