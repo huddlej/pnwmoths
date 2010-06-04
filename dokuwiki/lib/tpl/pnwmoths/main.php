f4f16811414425cb16fc2f89887429f05ec7031a
John Huddleston <huddlej@gmail.com>
2010-06-04 09:26:48 -0700

    added jquery colorbox plugin for viewing individual images in fullscreen mode.

--- dokuwiki/lib/tpl/pnwmoths/main.php
+++ dokuwiki/lib/tpl/pnwmoths/main.php
@@ -36,9 +36,11 @@ if (!defined('DOKU_INC')) die();
   <script language="javascript" type="text/javascript" src="/media/js/jqplot.canvasTextRenderer.min.js"></script>
   <script language="javascript" type="text/javascript" src="/media/js/jqplot.canvasAxisLabelRenderer.min.js"></script>
   <script language="javascript" type="text/javascript" src="/media/js/jqplot.categoryAxisRenderer.min.js"></script>
+  <script language="javascript" type="text/javascript" src="/media/js/jquery.colorbox-min.js"></script>
 
   <!--[if IE]><script language="javascript" type="text/javascript" src="/media/js/excanvas.min.js"></script><![endif]-->
   <link rel="stylesheet" href="/media/css/jquery.jqplot.css" type="text/css">
+  <link rel="stylesheet" href="/media/css/colorbox.css" type="text/css">
   <?php tpl_metaheaders()?>
 
   <link rel="shortcut icon" href="<?php echo DOKU_TPL?>images/favicon.ico" />
