665cc27daaf97814b029d2232d38fe8d7c06d500
John Huddleston <huddlej@gmail.com>
2010-06-08 11:46:01 -0700

    added js to hide all no-js class elements when js is enabled.

--- dokuwiki/lib/plugins/mothimages/script.js
+++ dokuwiki/lib/plugins/mothimages/script.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function() {
     // Remove styles for users without javascript.
     jQuery(".all-images").removeClass("all-images-no-js");
+    jQuery(".no-js").hide();
 
     if (jQuery("#other-images").length > 0) {
         jQuery("#other-images").jcarousel();
