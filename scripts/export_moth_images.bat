92431321039de0680b3a4ae5dfb97b1dcaeef41b
John Huddleston <huddlej@gmail.com>
2010-06-23 16:57:33 -0700

    removed unnecessary user input for image export script.

--- scripts/export_moth_images.bat
+++ scripts/export_moth_images.bat
@@ -1,6 +1,5 @@
 @echo off
 REM Converts each TIFF file in the moth images directory into a JPEG
 REM that is 800 pixels wide. Ignores files that have already been converted.
-set /p destination= In which directory should processed images be placed? 
 cd "C:\Images\Moths\"
-for /D %%i in (*) do for %%j in ("%%i/Final/*-?-?.tif") do if not exist "%destination%\%%~nj.jpg" "C:\Program Files (x86)\ImageMagick\convert.exe" -quiet "%%i\Final\%%j" -resize 800 "%destination%\%%~nj.jpg"
+for /D %%i in (*) do for %%j in ("%%i/Final/*-?-?.tif") do if not exist "C:\Images\Final Moths\%%~nj.jpg" "C:\Program Files (x86)\ImageMagick\convert.exe" -quiet "%%i\Final\%%j" -resize 800 "C:\Images\Final Moths\%%~nj.jpg"
