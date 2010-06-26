8372753f1bb942e68d0fbd34f6a4ef45d834394a
John Huddleston <huddlej@gmail.com>
2010-06-25 17:03:16 -0700

    added logic to updated images that already exist in the database and to add a modification date to all added or updated images.

--- scripts/import_images.php
+++ scripts/import_images.php
@@ -1,7 +1,13 @@
 <?php
 require_once '../www/bootstrap.php';
 
-$image_dir = "/home/huddlej/Desktop/moth-images-processed/test";
+// By default existing documents aren't updated.
+$update_docs = false;
+if (count($argv) > 1 && $argv[1] == "update_docs") {
+    $update_docs = true;
+}
+
+$image_dir = "/home/huddlej/Desktop/moths";
 $image_doc_template = array(
     "type" => "image",
     "author" => "John Huddleston",
@@ -17,38 +23,72 @@ $db = new Tillikum_CouchDb_Database($couchdb, $db_name);
 
 // Get all files after the current and parent directories (i.e., "." and "..").
 $files = array_slice(scandir($image_dir), 2);
+$images_saved = 0;
+
 foreach ($files as $file) {
     // Get species name from the file name.
     list($species, $rest) = explode("-", $file);
 
-    // Create a new image document based on the image document template and the
-    // species name. The document id is the file name.
-    $image_doc = new Tillikum_CouchDb_Document(
-        array_merge(
-            $image_doc_template,
-            array("_id" => $file,
-                  "species" => $species)
-        )
-    );
-
-    // Save image document.
     try {
-        $response = $db->saveDocument($image_doc);
+        // Look for an existing document with the same id to update.
+        $image_doc = $db->getDocument($file);
 
-        // Add image content to image document.
+        if ($update_docs) {
+            $revision = $image_doc->_rev;
+            $updated = true;
+        }
+        else {
+            continue;
+        }
+    }
+    catch (Tillikum_CouchDb_Exception $e) {
+        // Create a new image document based on the image document template and the
+        // species name. The document id is the file name.
+        $image_doc = new Tillikum_CouchDb_Document(
+            array_merge(
+                $image_doc_template,
+                array(
+                    "_id" => $file,
+                    "species" => $species
+                )
+            )
+        );
+        $response = $db->saveDocument($image_doc);
+        $revision = $response->rev;
+        $updated = false;
+    }
+
+    // Attach image file.
+    try {
+         // Add image content to image document.
         $body = file_get_contents(implode("/", array($image_dir, $file)));
         $attachment_response = $db->saveAttachment(
-            $response->id,
+            $image_doc->_id,
             $file,
             $body,
-            array("rev" => $response->rev),
+            array("rev" => $revision),
             $headers
         );
 
-        print "Added image '{$attachment_response->id}'\n";
+        // Save the date the document was modified after the image is
+        // successfully attached.
+        $image_doc = $db->getDocument($file);
+        $image_doc->date_modified = date("c");
+        $db->saveDocument($image_doc);
+        $images_saved++;
+
+        if ($updated) {
+            print "Updated image '{$attachment_response->id}'\n";
+        }
+        else {
+            print "Added image '{$attachment_response->id}'\n";
+        }
     }
     catch (Tillikum_CouchDb_Exception $e) {
-        print "Skipped existing image '$file'\n";
+        print "Skipped image '$file': " . print_r($e) . "\n";
     }
 }
+
+print "Found " . count($files) . " images.\n";
+print "Saved $images_saved images.\n";
 ?>
