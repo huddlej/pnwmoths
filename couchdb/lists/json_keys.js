629ca4cd0f64ba962a347540003259507dd01e67
John Huddleston <huddlej@gmail.com>
2010-06-30 16:54:23 -0700

    added check for undefined keys

--- couchdb/lists/json_keys.js
+++ couchdb/lists/json_keys.js
@@ -2,7 +2,9 @@ function (head, req) {
     provides("html", function () {
         var row, rows = [];
         while (row = getRow()) {
-            rows.push(row.key);
+            if (typeof(row.key) !== "undefined") {
+                rows.push(row.key);
+            }
         }
         send(toJSON(rows));
     });
