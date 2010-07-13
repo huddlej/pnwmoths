246ad8b928b11c8884537adb7dce03d1471f4cf9
John Huddleston <huddlej@gmail.com>
2010-07-13 15:43:27 -0700

    added a method to set similar species using a list of similar species compared against the existing list of similar species.

--- library/PNWMoths/Model/SimilarSpecies.php
+++ library/PNWMoths/Model/SimilarSpecies.php
@@ -75,7 +75,8 @@ class PNWMoths_Model_SimilarSpecies extends PNWMoths_Model {
             try {
                 $species = Zend_Json::encode($species);
                 $viewParams = array("reduce" => 'false',
-                                    "key" => $species);
+                                    "key" => $species,
+                                    "include_docs" => 'true');
                 $results = $db->getView(
                     self::$designDoc,
                     self::$viewName,
@@ -83,9 +84,9 @@ class PNWMoths_Model_SimilarSpecies extends PNWMoths_Model {
                 );
 
                 foreach ($results->rows as $row) {
-                    $similar_species[] = $row->value;
+                    $similar_species[$row->doc->_id . " " . $row->doc->_rev] = $row->value;
                 }
-                sort($similar_species);
+                asort($similar_species);
             }
             catch (Zend_Http_Client_Adapter_Exception $e) {
                 // Return no results when the database isn't available.
@@ -93,6 +94,48 @@ class PNWMoths_Model_SimilarSpecies extends PNWMoths_Model {
         }
 
         return $similar_species;
+    }
+
+    public static function setSimilarSpecies($species, $similar_species) {
+        // Get current similar species.
+        $current_similar_species = self::getSimilarSpecies($species);
+        $updated_docs = array();
+
+        // Add species that are in the given list and aren't in the existing
+        // list.
+        $new_species = array_diff($similar_species, $current_similar_species);
+        if (count($new_species) > 0) {
+            foreach ($new_species as $row) {
+                $updated_docs[] = new Tillikum_CouchDb_Document(
+                    array(
+                        "species" => $species,
+                        "similar_species" => $row,
+                        "type" => "similarspecies"
+                    )
+                );
+            }
+        }
+
+        // Remove species that aren't in the given list and are in the existing
+        // list.
+        $deleted_species = array_diff($current_similar_species, $similar_species);
+        if (count($deleted_species) > 0) {
+            foreach ($deleted_species as $id_rev => $row) {
+                list($id, $rev) = explode(" ", $id_rev);
+                $updated_docs[] = new Tillikum_CouchDb_Document(
+                    array(
+                        "_id" => $id,
+                        "_rev" => $rev,
+                        "_deleted" => true
+                    )
+                );
+            }
+        }
+
+        if(count($updated_docs) > 0) {
+            $db = parent::getDatabase();
+            $db->saveBulk($updated_docs);
+        }
     }
 }
 ?>
