02fff79072cca2411a2007f84f93abdb7b41617e
John Huddleston <huddlej@gmail.com>
2010-07-13 16:30:33 -0700

    updated by_similar_species couchdb view to avoid redundant records in the database.

--- couchdb/views/by_similar_species/map.js
+++ couchdb/views/by_similar_species/map.js
@@ -1,5 +1,7 @@
 function (doc) {
     if (doc.similar_species) {
+        // Emit a row for each pair to avoid redundant records in the database.
         emit(doc.species, doc.similar_species);
+        emit(doc.similar_species, doc.species);
     }
 }
