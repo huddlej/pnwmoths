113fede77bb1d5a2e66999fa8c5eb8ffb51443b8
John Huddleston <huddlej@gmail.com>
2010-07-29 11:11:14 -0700

    updated slideshow javascript and stylesheet to match new DOM elements.

--- www/media/slideshow/script.js
+++ www/media/slideshow/script.js
@@ -3,14 +3,14 @@ jQuery(document).ready(function() {
     jQuery(".all-images").removeClass("all-images-no-js");
     jQuery(".no-js").hide();
 
-    if (jQuery("#other-images").length > 0) {
-        jQuery("#other-images").jcarousel();
+    if (jQuery("#images .other-images").length > 0) {
+        jQuery("#images .other-images").jcarousel();
 
-        jQuery("#other-images a").click(function (event) {
+        jQuery("#images .other-images a").click(function (event) {
             event.preventDefault();
-            jQuery("#current-image a").replaceWith(jQuery(this).clone());
-            jQuery("#current-image a").colorbox();
+            jQuery("#images .current-image a").replaceWith(jQuery(this).clone());
+            jQuery("#images .current-image a").colorbox();
         });
-        jQuery("#current-image a").colorbox();
+        jQuery("#images .current-image a").colorbox();
     }
 });
