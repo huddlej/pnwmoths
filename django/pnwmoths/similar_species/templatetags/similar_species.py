86817c0bac2bf755e9b54c1bb0512d8679a95f36
John Huddleston <huddlej@gmail.com>
2010-08-06 15:57:44 -0700

    redefined similar_species template tag as an inclusion tag thus greatly simplifying the logic.

--- django/pnwmoths/similar_species/templatetags/similar_species.py
+++ django/pnwmoths/similar_species/templatetags/similar_species.py
@@ -4,10 +4,6 @@ Template tag for displaying similar species information.
 from couchdbkit import Server
 from django import template
 from django.conf import settings
-from django.template import Context, Node, Template, TemplateSyntaxError
-from django.template.loader import get_template
-
-from pnwmoths.couchdb_templatetags.templatetags.couchdb import get_content
 
 register = template.Library()
 
@@ -33,30 +29,16 @@ def get_similar_species(species):
     ))
 
 
-class SimilarSpeciesNode(Node):
-    def __init__(self, species):
-        self.species = species
-
-    def render(self, context):
-        # Get similar species.
-        similar_species = get_similar_species(self.species.resolve(context))
-
-        # Render similar species template and return the result.
-        template = get_template("similar_species/similar_species.html")
-        context = Context({"similar_species": similar_species})
-        return template.render(context)
-
-
-def similar_species(parser, token):
+def similar_species(context):
     """
     Returns an HTML slideshow of images for similar species of the given
     species.
     """
-    bits = token.split_contents()
-    if len(bits) != 2:
-        raise TemplateSyntaxError("'%s' takes exactly one argument"
-                                  " (name of the species to find similar species for)" % bits[0])
-    species = parser.compile_filter(bits[1])
-
-    return SimilarSpeciesNode(species)
-similar_species = register.tag(similar_species)
+    new_context = {}
+    current_page = context.get("current_page")
+    if current_page:
+        species_name = current_page.get_title()
+        new_context.update({"similar_species": get_similar_species(species_name),
+                            "request": context.get("request")})
+    return new_context
+similar_species = register.inclusion_tag("similar_species/similar_species.html", takes_context=True)(similar_species)
