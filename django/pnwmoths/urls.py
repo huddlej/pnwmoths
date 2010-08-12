c6a08850593b6f407e9afd27184421d63ad365fe
John Huddleston <huddlej@gmail.com>
2010-08-12 10:39:41 -0700

    added a custom login form for PNW Moths site.

--- django/pnwmoths/urls.py
+++ django/pnwmoths/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
     (r'^cushion/', include('pnwmoths.cushion.urls')),
     (r'^admin/', include(admin.site.urls)),
     #url(r"^contact-us/", contact_us, name="contact-us"),
