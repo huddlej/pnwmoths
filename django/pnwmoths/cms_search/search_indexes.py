b37e8a821ec2941ef8eb1762b54fd9bf112b7c47
John Huddleston <huddlej@gmail.com>
2010-08-13 08:01:37 -0700

    added a doc string to explain the custom queryset used by cms search index.

--- django/pnwmoths/cms_search/search_indexes.py
+++ django/pnwmoths/cms_search/search_indexes.py
@@ -8,6 +8,11 @@ class PageIndex(indexes.SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
 
     def get_queryset(self):
+        """
+        Limits search results to pages that have been published and either don't
+        have start and end dates or the current time is between the start and
+        end dates.
+        """
         queryset = Page.objects.filter(published=True)
         not_expired = Q(publication_end_date__gt=datetime.datetime.now()) | Q(publication_end_date__isnull=True)
         already_published = Q(publication_date__lte=datetime.datetime.now()) | Q(publication_date__isnull=True)
