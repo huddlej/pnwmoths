a4e49e709887649d9c56fc9b6844eee21c7735c9
John Huddleston <huddlej@gmail.com>
2010-08-12 21:11:34 -0700

    added search to the site's base template, a search results page, and a search url.

--- django/pnwmoths/urls.py
+++ django/pnwmoths/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     (r'^accounts/login/$', 'django.contrib.auth.views.login'),
     (r'^cushion/', include('pnwmoths.cushion.urls')),
     (r'^admin/', include(admin.site.urls)),
+    url(r'^search/', include('haystack.urls'), name="search"),
     #url(r"^contact-us/", contact_us, name="contact-us"),
     url(r'^', include('cms.urls')),
 )
