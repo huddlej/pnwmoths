3fbfeb0b93a185816b13b53dae924a2034a85cf3
John Huddleston <huddlej@gmail.com>
2010-08-27 12:15:36 -0700

    fixed a bug with the by_species_image reduce code when there are no records returned by map.

--- couchdb/views/by_species_image/reduce.js
+++ couchdb/views/by_species_image/reduce.js
@@ -1,14 +1,19 @@
 function (key, values) {
-    values.sort(function (a, b) {
-        if (a.attachment < b.attachment) {
-            return -1;
-        }
-        else if (a.attachment > b.attachment) {
-            return 1;
-        }
-        else {
-            return 0;
-        }
-    });
-    return values[0];
+    if (values.length > 0) {
+        values.sort(function (a, b) {
+            if (a.attachment < b.attachment) {
+                return -1;
+            }
+            else if (a.attachment > b.attachment) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        });
+        return values[0];
+    }
+    else {
+        return null;
+    }
 }
