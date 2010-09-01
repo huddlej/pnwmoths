c1a171c2c54edb77854dbee30dbb9060c28c32c8
John Huddleston <huddlej@gmail.com>
2010-09-01 11:25:54 -0700

    updated library path

--- www/bootstrap.php
+++ www/bootstrap.php
@@ -7,7 +7,7 @@ $paths = array(
     "/usr/local/share/ZendFramework/library",
     "/usr/share/php/libzend-framework-php",
     "/home/huddlej/src/tillikum-couchdb/library",
-    "/home/huddlej/pnwmoths/library",
+    "/usr/local/www/pnwmoths/library",
     get_include_path()
 );
 
