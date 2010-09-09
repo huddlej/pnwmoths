467fc7cd876fc061b4132c2a0b1d29f0ee449572
John Huddleston <huddlej@gmail.com>
2010-09-09 11:58:12 -0700

    updated couchdb full_images list to work with new labelled species image view.

--- couchdb/lists/full_images.js
+++ couchdb/lists/full_images.js
@@ -3,9 +3,9 @@ function (head, req) {
     // !code vendor/mustache.js
     provides("html", function () {
         var row,
-            attachment,
             first_row = true,
-            view;
+            view,
+            title;
 
         if (req.query.first_image) {
             first_image = true;
@@ -15,17 +15,29 @@ function (head, req) {
         }
 
         while (row = getRow()) {
-            view = {"image_src": req.query.image_url,
-                    "image_name": row.value.id,
-                    "title": row.value.caption || "",
-                    "first_row": first_row,
-                    "first_image": first_image,
-                    "show_title": req.query.show_title,
-                    "species": row.key};
-            send(Mustache.to_html(templates.lists.full_images.row, view));
+            // The first row in the collated set is a label whose value can be
+            // used for the next rows' titles.
+            if (row.key[2] == 0) {
+                title = row.value.replace("\n", ", ");
+            }
+            else {
+                view = {"image_src": req.query.image_url,
+                        "image_name": row.value.id,
+                        "first_row": first_row,
+                        "first_image": first_image,
+                        "show_title": req.query.show_title,
+                        "species": row.key[0]};
+                if (row.value.caption) {
+                    view["title"] = "caption: " + row.value.caption + ", " + title;
+                }
+                else {
+                    view["title"] = title;
+                }
+                send(Mustache.to_html(templates.lists.full_images.row, view));
 
-            if (first_row) {
-                first_row = false;
+                if (first_row) {
+                    first_row = false;
+                }
             }
         }
 
