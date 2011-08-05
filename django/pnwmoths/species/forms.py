06bcd3697bc9bea8bf10b4828772159d62559dad
John Huddleston <huddlej@gmail.com>
2011-08-04 21:35:07 -0700

    deleted couchdb-specific code and added django-specific code.

--- django/pnwmoths/species/forms.py
+++ django/pnwmoths/species/forms.py
@@ -17,7 +17,7 @@ class ImportSpeciesRecordsForm(forms.Form):
                             "tab": '\t',
                             "space": ' '}
     file = forms.FileField(label="Select a data file:")
-    model = forms.ChoiceField(choices=MODEL_CHOICES, required=False)
+    model = forms.ChoiceField(choices=MODEL_CHOICES)
     overwrite = forms.BooleanField(label="Overwrite existing records", required=False)
     skip_duplicates = forms.BooleanField(
         required=False,
@@ -37,6 +37,8 @@ class ImportSpeciesRecordsForm(forms.Form):
         Parses the given file object as a CSV file and adds the contents to the
         given database using the first row as attribute names for each column.
         """
+        model = self.cleaned_data["model"]
+        #model = registered_models.get(self.cleaned_data["model"])
         reader = csv.reader(file, delimiter=self.cleaned_data["delimiter"])
         errors = []
         docs = []
@@ -54,92 +56,13 @@ class ImportSpeciesRecordsForm(forms.Form):
                         for i in column_range
                         if len(row[i]) > 0])
 
-            if self.cleaned_data["model"]:
-                model = registered_models.get(self.cleaned_data["model"])
-                try:
-                    doc = model.coerce(doc)
-                    doc = model(**doc)
-                    # doc["type"] = self.cleaned_data["model"].lower()
-                    docs.append(doc)
-                except ValueError, e:
-                    errors.append((doc, traceback.format_exc()))
-            else:
-                docs.append(doc)
-
-        # Only try to save documents if there weren't any errors.
-        if len(errors) == 0:
-            # Check for existing documents with the same ids as the imported
-            # documents.
-            keys = [doc.get_id for doc in docs if getattr(doc, "get_id", None)]
-            existing_docs = self.existing_docs(database, keys, docs)
-
-            if len(existing_docs) > 0:
+            try:
+                coerced_doc = model.coerce(doc)
                 if self.cleaned_data["overwrite"]:
-                    # If the user approved document overwriting, update each
-                    # document of imported data to the current revision of that
-                    # document in the database.
-                    docs = existing_docs
+                    instance, created = model.objects.get_or_create(**coerced_doc)
                 else:
-                    # If the user didn't approve document overwriting, return an
-                    # error list for the conflicting documents.
-                    errors = [(existing_doc, "Document already exists.")
-                              for existing_doc in existing_docs]
-
-            if len(errors) == 0:
-                try:
-                    database.bulk_save([getattr(doc, "_doc", None) or doc
-                                        for doc in docs])
-                except BulkSaveError, e:
-                    if not self.cleaned_data["skip_duplicates"]:
-                        # Show the difference between apparently duplicated
-                        # records.
-                        keys = set([error["id"] for error in e.errors])
-                        docs_by_id = {}
-                        for doc in docs:
-                            doc_id = doc._doc["_id"]
-                            if doc_id in keys:
-                                docs_by_id.setdefault(doc_id, []).append(doc._doc)
-
-                        for key in keys:
-                            if key in docs_by_id:
-                                errors.append((
-                                    self.diff_docs(docs_by_id[key][0], docs_by_id[key][1]),
-                                    "Duplicate document; differences between documents shown."
-                                ))
+                    instance = model.objects.create(**coerced_doc)
+            except ValueError, e:
+                errors.append((coerced_doc, ))
 
         return errors
-
-    def diff_docs(self, doc_a, doc_b):
-        """
-        Return a unified diff of the given docs' string representations.
-        """
-        return "\n".join(
-            list(
-                difflib.unified_diff(
-                    pprint.pformat(doc_a).split("\n"),
-                    pprint.pformat(doc_b).split("\n"),
-                    lineterm=""
-                )
-            )
-        )
-
-    def existing_docs(self, database, ids, docs=None):
-        existing_docs = database.documents(keys=ids)
-        existing_docs = filter(lambda x: "error" not in x, existing_docs)
-
-        # If no documents are given to manipulate, return the documents fetched
-        # from the database. If documents are given and exist in the database,
-        # update given documents with the revisions of the documents in the
-        # database. Otherwise there is nothing to do.
-        if docs is None:
-            docs = existing_docs
-        elif len(existing_docs) > 0:
-            revisions_by_id = dict([(doc["id"], doc["value"]["rev"])
-                                    for doc in existing_docs])
-            for doc in docs:
-                if "_id" in doc and doc["_id"] in revisions_by_id:
-                    doc._doc["_rev"] = revisions_by_id[doc["_id"]]
-        else:
-            docs = []
-
-        return docs
