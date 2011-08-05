e2999ce0210ed99ddf64dddd89242c1ec299071c
John Huddleston <huddlej@gmail.com>
2011-08-04 21:32:29 -0700

    replaced model-specific router with a database-specific router.

--- django/pnwmoths/routers.py
+++ django/pnwmoths/routers.py
@@ -1,30 +1,31 @@
-class SpeciesRouter(object):
+class MySqlRouter(object):
     """
-    A router to control all database operations on models in the species
-    application.
+    A router to control all database operations on models in the MySQL database.
     """
+    app_labels = ["species"]
+
     def db_for_read(self, model, **hints):
-        "Point all operations on species models to 'pnwmoths'"
-        if model._meta.app_label == 'species':
+        "Point all operations on selected models to 'pnwmoths'"
+        if model._meta.app_label in self.app_labels:
             return 'pnwmoths'
         return None
 
     def db_for_write(self, model, **hints):
         "Point all operations on species models to 'pnwmoths'"
-        if model._meta.app_label == 'species':
+        if model._meta.app_label in self.app_labels:
             return 'pnwmoths'
         return None
 
     def allow_relation(self, obj1, obj2, **hints):
         "Allow any relation if a model in species is involved"
-        if obj1._meta.app_label == 'species' or obj2._meta.app_label == 'species':
+        if obj1._meta.app_label in self.app_labels or obj2._meta.app_label in self.app_labels:
             return True
         return None
 
     def allow_syncdb(self, db, model):
         "Make sure the species app only appears on the 'pnwmoths' db"
         if db == 'pnwmoths':
-            return model._meta.app_label == 'species'
-        elif model._meta.app_label == 'species':
+            return model._meta.app_label in self.app_labels
+        elif model._meta.app_label in self.app_labels:
             return False
         return None
--- django/pnwmoths/settings_global.py
+++ django/pnwmoths/settings_global.py
@@ -30,7 +30,7 @@ DATABASES = {
     }
 }
 
-DATABASE_ROUTERS = ["pnwmoths.routers.SpeciesRouter"]
+DATABASE_ROUTERS = ["pnwmoths.routers.MySqlRouter"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
