bfb12743282a7c9ff35ff465c8bf6c42e4e9b226
John Huddleston <huddlej@gmail.com>
2011-08-08 21:34:10 -0700

    added a unit test for data import

--- django/pnwmoths/species/tests.py
+++ django/pnwmoths/species/tests.py
@@ -1,23 +1,41 @@
 """
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
+Species application tests.
 """
+import csv
+import os
 
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from forms import ImportSpeciesRecordsForm
+from models import SpeciesRecord
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
 
->>> 1 + 1 == 2
-True
-"""}
+class TestImportSpeciesRecordsForm(TestCase):
+    fixtures = ["species.json",
+                "collectors.json",
+                "collections.json",
+                "counties.json",
+                "states.json"]
+
+    def setUp(self):
+        self.model = SpeciesRecord
+        path, current_file = os.path.split(os.path.abspath(__file__))
+        filename = os.path.join(path, "test_data.csv")
+        self.file = open(filename, "r")
+        self.delimiter = ","
+
+    def tearDown(self):
+        self.file.close()
+
+    def test_import_data(self):
+        """
+        Tests import of CSV data into a given model.
+
+        import_data(self, model, file, delimiter, overwrite=False, dry_run=True):
+        """
+        form = ImportSpeciesRecordsForm()
+        results, errors = form.import_data(self.model, self.file, self.delimiter)
+        self.assertEqual(0, len(errors))
+        self.assertTrue(len(results) > 0)
+        self.assertTrue(isinstance(results[0], model))
 
