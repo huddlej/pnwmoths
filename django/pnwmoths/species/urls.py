dad76c18bc51392ddff4e0826ba32d7e7cead104
John Huddleston <huddlej@gmail.com>
2011-08-17 22:20:27 -0700

    registered collection resource

--- django/pnwmoths/species/urls.py
+++ django/pnwmoths/species/urls.py
@@ -2,14 +2,14 @@ from tastypie.api import Api
 
 from django.conf.urls.defaults import include, patterns, url
 
-from resources import CountyResource, SpeciesResource, StateResource
+from resources import CollectionResource, CountyResource, SpeciesResource, StateResource
 
 data_api = Api(api_name="api")
+data_api.register(CollectionResource())
 data_api.register(CountyResource())
 data_api.register(SpeciesResource())
 data_api.register(StateResource())
 
 urlpatterns = patterns('',
     url(r'', include(data_api.urls))
-#    url(r'^species/(?P<pk>[^/]+)/$', InstanceModelView.as_view(resource=SpeciesResource)),
 )
