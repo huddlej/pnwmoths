fab850a0bd216a76cedba6887de21d1b9b7443b1
John Huddleston <huddlej@gmail.com>
2011-08-28 21:33:26 -0700

    added thumbnail mixin to SpeciesImage admin model which generates thumbnails for display in the django admin.

--- django/pnwmoths/species/admin.py
+++ django/pnwmoths/species/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
+from sorl.thumbnail.admin import AdminImageMixin
 
 from models import (Collection, Collector, County, Species, SpeciesImage,
                     SpeciesRecord, State)
+
+
+class SpeciesImageAdmin(AdminImageMixin, admin.ModelAdmin):
+    pass
+admin.site.register(SpeciesImage, SpeciesImageAdmin)
 
 
 admin.site.register(Collection)
 admin.site.register(Collector)
 admin.site.register(County)
 admin.site.register(Species)
-admin.site.register(SpeciesImage)
 admin.site.register(SpeciesRecord)
 admin.site.register(State)
