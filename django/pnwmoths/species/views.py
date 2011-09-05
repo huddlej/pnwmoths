fbc7da8bba89e3f72b60ee1eeb46d3235e5fe6f9
John Huddleston <huddlej@gmail.com>
2011-09-05 13:20:18 -0700

    added the simplest possible transform function for csvimporter.

--- django/pnwmoths/species/views.py
+++ django/pnwmoths/species/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from forms import ImportSpeciesRecordsForm
-from models import SpeciesRecord
+from models import Species, SpeciesRecord
 
 
 IMPORT_ERROR_MESSAGE = """There was a problem with one or more rows in your
@@ -40,3 +40,21 @@ def import_species_records(request):
     return render_to_response("admin/import.html",
                               context,
                               context_instance=RequestContext(request))
+
+
+def transform_species_record(request, data):
+    """
+    Transforms a CSV record into a species record.
+    """
+    species, created = Species.objects.get_or_create(
+        genus=data["genus"],
+        species=data["species"]
+    )
+
+    new_data = {
+        "species": species,
+        "latitude": data.get("latitude") or 0,
+        "longitude": data.get("longitude") or 0
+    }
+
+    return new_data
