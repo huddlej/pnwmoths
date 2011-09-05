f9890ef830b29940fd91bf23c67cfec1ecb8787b
John Huddleston <huddlej@gmail.com>
2011-09-05 14:27:45 -0700

    added custom admin settings for SpeciesRecords

--- django/pnwmoths/species/admin.py
+++ django/pnwmoths/species/admin.py
@@ -10,9 +10,27 @@ class SpeciesImageAdmin(AdminImageMixin, admin.ModelAdmin):
 admin.site.register(SpeciesImage, SpeciesImageAdmin)
 
 
+class SpeciesRecordAdmin(admin.ModelAdmin):
+    list_display = (
+        "species",
+        "latitude",
+        "longitude",
+        "state",
+        "county",
+        "collection",
+        "collector"
+    )
+    list_filter = (
+        "state",
+        "county",
+        "collection"
+    )
+    search_fields = ("species__genus", "species__species")
+admin.site.register(SpeciesRecord, SpeciesRecordAdmin)
+
+
 admin.site.register(Collection)
 admin.site.register(Collector)
 admin.site.register(County)
 admin.site.register(Species)
-admin.site.register(SpeciesRecord)
 admin.site.register(State)
