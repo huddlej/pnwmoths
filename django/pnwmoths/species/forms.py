9c77aa9b0912de6a8ee646dedb4982552c0b6ee8
John Huddleston <huddlej@gmail.com>
2011-10-27 22:13:54 -0700

    added validation for form fields.

--- django/pnwmoths/species/forms.py
+++ django/pnwmoths/species/forms.py
@@ -2,23 +2,87 @@ import csv
 
 from django import forms
 
-from models import SpeciesRecord
+from models import Collection, Collector, County, Species, SpeciesRecord, State
 
 registered_models = {"SpeciesRecord": SpeciesRecord}
 
 
 class SpeciesRecordForm(forms.ModelForm):
     genus = forms.CharField()
-    species = forms.CharField()
-    city = forms.CharField()
-    collector = forms.CharField()
-    collection = forms.CharField()
-    county = forms.CharField()
-    state = forms.CharField()
-    notes = forms.CharField()
+    species = forms.CharField(required=False)
+    city = forms.CharField(required=False)
+    collector = forms.CharField(required=False)
+    collection = forms.CharField(required=False)
+    county = forms.CharField(required=False)
+    state = forms.CharField(required=False)
+    notes = forms.CharField(required=False)
 
     class Meta:
         model = SpeciesRecord
+        exclude = ("county",)
+
+    def clean_collection(self):
+        value = self.cleaned_data.get("collection") or None
+
+        if value:
+            try:
+                value = Collection.objects.get(name=value)
+            except Collection.DoesNotExist, e:
+                raise forms.ValidationError(e.message)
+
+        return value
+
+    def clean_collector(self):
+        value = self.cleaned_data.get("collector") or None
+
+        if value:
+            try:
+                value = Collector.objects.get(name=value)
+            except Collector.DoesNotExist, e:
+                raise forms.ValidationError(e.message)
+
+        return value
+
+    def clean_state(self):
+        value = self.cleaned_data.get("state") or None
+
+        if value:
+            try:
+                value = State.objects.get(code=value)
+            except State.DoesNotExist, e:
+                raise forms.ValidationError(e.message)
+
+        return value
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        try:
+            species_instance = Species.objects.get(
+                genus=cleaned_data.get("genus"),
+                species=cleaned_data.get("species")
+            )
+            cleaned_data["species"] = species_instance
+        except Species.DoesNotExist, e:
+            del self.cleaned_data["species"]
+            raise forms.ValidationError(e.message)
+
+        if cleaned_data.get("county"):
+            if cleaned_data.get("state"):
+                try:
+                    cleaned_data["county"] = County.objects.get(
+                        name=cleaned_data.get("county"),
+                        state=cleaned_data.get("state")
+                    )
+                except County.DoesNotExist, e:
+                    del self.cleaned_data["county"]
+                    raise forms.ValidationError("County doesn't exist.")
+            else:
+                del cleaned_data["county"]
+
+        cleaned_data["locality"] = cleaned_data.get("city")
+
+        return cleaned_data
 
 
 class ImportSpeciesRecordsForm(forms.Form):
