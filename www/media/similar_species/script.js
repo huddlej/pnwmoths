4c76b92cc7e78ac3700cea50e52d1ef3c8c743d6
pbjorge <philipbjorge@gmail.com>
2011-11-29 13:50:26 -0800

    Fix #3. Fixed indentation before pull request

--- www/media/similar_species/script.js
+++ www/media/similar_species/script.js
@@ -2,6 +2,6 @@ jQuery(document).ready(function() {
     if (jQuery("#similar .other-images").length > 0) {
         jQuery("#similar .other-images").jcarousel({
             scroll: 2
-	});
+        });
     }
 });
