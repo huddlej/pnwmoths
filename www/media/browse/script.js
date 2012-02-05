70e4491397eb318f15439d340f742abe886a44b9
Philip Bjorge <philipbjorge@gmail.com>
2012-02-05 15:33:30 -0800

    Implemented in-place nested loading.

--- www/media/browse/script.js
+++ www/media/browse/script.js
@@ -1,26 +1,76 @@
+(function($) {
+$.fn.implement_browse = function () {
+    // Set width to fix jerky slidetoggle
+    // +1 to account for #bd's children
+    var parentOffset = jQuery(this).parents('.browse_item').length + 1; 
+    jQuery(this).width(jQuery('#bd').width() - 10*parentOffset);
+    // set h3 size to improve nesting
+    if (jQuery(this).parents('.browse_item').length) {
+        var curFontSize = jQuery(this).parents('.browse_item').find('h3').css('fontSize');
+        var finalNum = parseFloat(curFontSize, 10)/1.2;
+        var stringEnding = curFontSize.slice(-2);
+        jQuery(this).find('h3').css('fontSize', finalNum + stringEnding);
+    }
+
+    // Add collapsing arrow to div
+    jQuery(this).find('.browse_heading').append('<img src="http://i.imgur.com/ETq1Y.png" class="toggle_item" />');
+    // Give the arrow toggle states
+    jQuery(this).find('.toggle_item').toggle(
+        function(){ jQuery(this).attr("src", "http://i.imgur.com/WtKF5.png"); },
+        function(){ jQuery(this).attr("src", "http://i.imgur.com/ETq1Y.png"); }
+    );
+
+    // Toggle on click
+    jQuery(this).find('.toggle_item').click(function() {
+        if (jQuery(this).parent().siblings('.ajax').html().length) {
+            jQuery(this).parent().siblings('.browse_thumbs').slideToggle('slow');
+            jQuery(this).parent().siblings('.ajax').slideToggle('slow');
+        } else {
+            // AJAX Load
+            var url = jQuery(this).parent().find('h3 a').attr('href');
+            jQuery(this).parent().siblings('.ajax').load(url + ' .browse_item', function() {
+
+                jQuery(this).siblings('.browse_thumbs').slideToggle('slow');
+                // BASICALLY, WE ADD +/- AND FUNCTIONALITY
+                // Apply THIS_FUNC() to jQuery(this) and below
+                jQuery(this).append('<div style="height: 0px; clear: both;">&nbsp;</div>');
+                jQuery(this).find('.browse_item:not(.species)').implement_browse();
+                jQuery(this).implement_species_width();
+                jQuery(this).slideToggle('slow');
+                return false;
+            });
+        }
+    });
+
+}
+
+$.fn.implement_species_width = function() {
+    var count = jQuery(this).children('.species').length;
+    if (count <= 6) {
+        jQuery(this).children('.species').width('46%');
+    }
+    else if (count > 6) {
+        jQuery(this).children('.species').width('30%');
+    }
+    else {
+        jQuery(this).children('.species').width('23%');
+    }
+}
+
+})(jQuery);
+
+
 jQuery(document).ready(function() {
-    // Make entire div clickable using first link in the container
-    jQuery(".browse_item").click(function(){
+    jQuery('#bd').find('.browse_item:not(.species)').implement_browse();
+
+    //Make species div clickable using first link in the container
+    jQuery(".ajax").delegate(".species" ,"click", function(){
         window.location=jQuery(this).find("h3 a").attr("href");
         return false;
     });
 
-    // Set div's hover effect
-    jQuery(".browse_item").hover(function() {
+    // Set species div's hover effect
+    jQuery(".ajax").delegate(".species", "hover", function() {
         jQuery(this).toggleClass('browse_item_hover');
     });
-
-    // Add collapsing arrow to div
-    jQuery('.browse_item:not(.species) .browse_heading').append('<img src="http://i.imgur.com/R5I0z.jpg" class="toggle_item" />');
-    // Give the arrow toggle states
-    jQuery('.browse_item .browse_heading .toggle_item').toggle(
-        function(){ jQuery(this).attr("src", "http://i.imgur.com/OiJKx.jpg"); },
-        function(){ jQuery(this).attr("src", "http://i.imgur.com/R5I0z.jpg"); }
-    );
-    // Toggle on click
-    jQuery('.browse_item .browse_heading .toggle_item').click(function() {
-        jQuery(this).parent().siblings('.browse_thumbs').slideToggle('slow');
-        return false;
-    });
-
 });
