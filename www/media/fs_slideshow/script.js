5704813a21849415e5865719789be51263adb16a
philipbjorge <philipbjorge@gmail.com>
2012-05-10 12:27:22 +0200

    Removed some inline styles. Fixed zoomify in IE.

--- www/media/fs_slideshow/script.js
+++ www/media/fs_slideshow/script.js
@@ -35,32 +35,35 @@ jQuery(document).ready(function() {
 
         // Check for a zoomified image to initialize the viewer with
 
-        var initZ = jQuery(".inline-species-image").first();
-        if (!initZ.hasClass("missing_zoomify")) {
+        var initZ = jQuery(".inline-species-image:not(.missing_zoomify)").first();
+        if (initZ.length) {
             jQuery("#zoomify-factsheet").removeClass("hidden");
-            Z.showImage("zoomify-factsheet", initZ.text(), "zToolbarVisible=1&zNavigatorVisible=2&zSkinPath=/media/zoomify/Skins/Default");
-            var h = jQuery("#zoomify-factsheet").width() * (2/3);
-            jQuery("#zoomify-factsheet").height(h+21);
-
-        } else {
-            initZ = jQuery(".inline-species-image:not(.missing_zoomify)").first();
-            jQuery("#zoomify-factsheet").removeClass("hidden");
-            Z.showImage("zoomify-factsheet", initZ.text(), "zToolbarVisible=1&zNavigatorVisible=2&zSkinPath=/media/zoomify/Skins/Default");
             jQuery("#zoomify-factsheet").width(jQuery("#zoomify-factsheet").width());
             var h = jQuery("#zoomify-factsheet").width() * (2/3);
-            jQuery("#zoomify-factsheet").height(h+21);
+            jQuery("#zoomify-factsheet").height(h+1);
 
-            jQuery("#zoomify-factsheet").addClass("hidden");
-            jQuery("#old-image-factsheet").removeClass("hidden");
-            jQuery("#old-image-factsheet a").colorbox();
+            // Temporary fix until navigator is fixed in IE
+            if (jQuery.browser.msie)
+                Z.showImage("zoomify-factsheet", initZ.text(), "zToolbarVisible=5&zNavigatorVisible=0&zSkinPath=/media/zoomify/Skins/Default");
+            else
+                Z.showImage("zoomify-factsheet", initZ.text(), "zToolbarVisible=5&zNavigatorVisible=2&zSkinPath=/media/zoomify/Skins/Default");
+
+            if (jQuery(".inline-species-image").first().hasClass("missing_zoomify")) {
+                jQuery("#zoomify-factsheet").addClass("hidden");
+                jQuery("#old-image-factsheet").removeClass("hidden");
+                jQuery("#old-image-factsheet a").colorbox();
+            }
         }
 
+        // Copyright for zoomify is hacked into to the zoomify script
+        // grep for "Philip Bjorge"
         jQuery(document).bind('cbox_open', function() {
              jQuery("#slideshow-copyright").toggleClass("fullscreen_copyright");
         });
         jQuery(document).bind('cbox_closed', function() {
              jQuery("#slideshow-copyright").toggleClass("fullscreen_copyright");
         });
+
         jQuery(window).resize();
     }
 });
