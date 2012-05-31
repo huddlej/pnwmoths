e20d4f623389091be6652482f9eef241e89d2823
Philip Bjorge <philipbjorge@gmail.com>
2012-05-30 20:23:19 -0400

    Modified label exporting to better match our CSVs.

--- django/pnwmoths/species/actions.py
+++ django/pnwmoths/species/actions.py
@@ -29,6 +29,7 @@ def export_labels_as_csv_action(description="Export Labels as CSV"):
 
         for obj in queryset:
             # for each label's image:
+            already_added = set()
             for im in obj.speciesimage_set.all():
                 d = [unicode(getattr(obj, field, "")
                                          if getattr(obj, field) is not None
@@ -36,8 +37,14 @@ def export_labels_as_csv_action(description="Export Labels as CSV"):
                                  for field in field_names]
                 # adds filename value                
                 # basename grabs the filename, splitext seperates the extension
-                d.insert(0, splitext(basename(im.image.name))[0])
-                writer.writerow(d)
+                # [:-1] pulls off the V/D distinction and then adds label
+                # We then export one row for each id, with multiple image pairs
+                # in the format Genus species-A-label
+                merged = splitext(basename(im.image.name))[0][:-1] + "label"
+                if merged not in already_added:
+                    already_added.add(merged)
+                    d.insert(0, merged)
+                    writer.writerow(d)
 
         return response
 
