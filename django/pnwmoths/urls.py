511a77c48b80a404c4cc0f834fa58a28a0b08143
Philip Bjorge <philipbjorge@gmail.com>
2012-07-19 00:25:28 -0400

    Redirect lucid key help to local page.

--- django/pnwmoths/urls.py
+++ django/pnwmoths/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.defaults import (
     patterns,
     url
 )
+from django.http import HttpResponseRedirect
 from django.contrib import admin
 
 from species.views import import_species_records, photographic_plate_zoomify
@@ -23,6 +24,7 @@ urlpatterns = patterns('',
     (r'^admin/lookups/', include(ajax_select_urls)),
     (r'^admin/', include(admin.site.urls)),
     url(r'^search/', FancyRedirectSearchView(), name="search"),
+    (r'^identify/lucid_player/help/default.htm$', lambda x: HttpResponseRedirect('/explore-data/about-key/')),
     (r'^photographic-plates/(?P<plate_pk>\d+)/$', 'species.views.photographic_plate_zoomify'),
     (r'^admin_sentry/', include('admin_sentry.urls')),
     url(r'^', include('cms.urls')),
