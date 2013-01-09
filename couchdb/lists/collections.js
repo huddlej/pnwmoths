4af383fa3fb44e6d647ca400773b332f7aad3747
philipbjorge <philipbjorge@gmail.com>
2013-01-09 14:54:11 -0800

    Cleaned up unused files. Removed outdated INSTALL (working on wiki updates).

--- couchdb/lists/collections.js
+++
@@ -1,8 +0,0 @@
-function (head, req) {
-    provides("html", function () {
-        var row;
-        while (row = getRow()) {
-            send(row.key + "<br />");
-        }
-    });
-}
--- couchdb/views/by_species_image/map.js
+++
@@ -1,6 +0,0 @@
-function (doc) {
-    if (doc.type == "image" && doc.species) {
-        emit(doc.species, {"id": doc._id,
-                           "caption": doc.caption || ""});
-    }
-}
